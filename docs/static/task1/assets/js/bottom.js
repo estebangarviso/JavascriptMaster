8b0175bbfbccfd955c7d4b7d53d74aaaaacf7972
Esteban Garviso <e.garvisovenegas@gmail.com>
2021-07-29 04:59:29 -0400

    update

--- docs/static/task1/assets/js/bottom.js
+++ docs/static/task1/assets/js/bottom.js
@@ -1,12 +1,12 @@
 /*!
  * Copyright 2021 Esteban Garviso
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+ * Licensed under MIT (https://github.com/estebangarviso/JavascriptMaster/blob/main/LICENSE.md)
  */
 document.addEventListener('DOMContentLoaded', function (e){
     "use strict"; // Force variables to be declarated
     //  Form Floating Labels JS
     document.querySelectorAll('.floating-label-form-group').forEach(el => {
-        el.addEventListener('keyup', (ev) => {
+        el.addEventListener('input', (ev) => {
             let cT = ev.currentTarget,
                 t = ev.target;
             if(t.value && t.value.length > 0)
@@ -14,8 +14,9 @@ document.addEventListener('DOMContentLoaded', function (e){
         });
         el.addEventListener('change', (ev) => {
             let cT = ev.currentTarget,
-                t = ev.target;
-            if(!t.value && t.value.length === 0)
+                t = ev.target,
+                v = t.tagName === 'SELECT' ? t.options[t.selectedIndex].getAttribute('value') : t.value;
+            if(!v || v.length === 0)
                 cT.classList.remove('floating-label-form-group-with-value');
         });
         el.addEventListener('focus', (ev) => {
@@ -28,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function (e){
         }, true);
     });
     //  Show/Hide Password JS
-    document.querySelectorAll('input[type=password]').forEach((el, i, nl) => {
+    document.querySelectorAll('.js-visible-password').forEach((el, i, nl) => {
         let n = nl[i],
             input = n.parentElement.querySelector('.js-visible-password'),
             btn = n.parentElement.querySelector('.js-show-password');
@@ -45,11 +46,16 @@ document.addEventListener('DOMContentLoaded', function (e){
             }
         });
     });
-
+    //  Dynamic Register Form Validation Settings
+    if(Validate.isLoadedObject(RegisterForm)){
+        let registerForm = document.getElementById(RegisterForm.id),
+            inputs = registerForm.querySelectorAll('input');
+        //console.log(inputs);
+    }
 });
 //Please enter your firstname.
 //Please enter your lastname.
 //Please enter your email address.
 //Please enter your password.
 //Please confirm your password.
-//Please accept the privacy terms before register
+//Please accept the privacy terms before register.
